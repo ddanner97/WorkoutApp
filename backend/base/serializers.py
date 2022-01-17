398e25c67fb5f29055344f6611e0981799d01fcf
Damian Danner <Damian@Lexis-MacBook-Pro.local>
2022-01-17 14:42:27 -0600

    Add backend user authentication and login

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,7 +1,37 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import Program, Routine, Exercise, ExerciseRoutine, WorkoutParameter
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', 'isAdmin']
+
+    def get_name(self, object):
+        name = object.first_name + ' ' + object.last_name
+        if name == '':
+            name = object.email
+
+        return name
+    
+    def get_isAdmin(self, object):
+        return object.is_staff
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', 'isAdmin', 'token'] 
+
+    def get_token(self, object):
+        token = RefreshToken.for_user(object)
+        return str(token.access_token)
 
 class ProgramSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,21 +1,76 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 
+# Customizing JWT Token
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+# Password Hash and Error status for registration
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+#import Django User Model
+from django.contrib.auth.models import User
+
 from .models import *
-from .serializers import ProgramSerializer, RoutineSerializer, ExerciseRoutineSerializer, ExerciseSerializer, WorkoutParameterSerializer 
+from .serializers import ProgramSerializer, RoutineSerializer, ExerciseRoutineSerializer, ExerciseSerializer, WorkoutParameterSerializer, UserSerializer, UserSerializerWithToken
 
-# Create your views here.
+#Custom JWT Token Views
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+#Register User
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            username=data['username'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+
+        return Response(serializer.data)
+
+    except:
+        message = {'detail': 'User with this email or username already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+#USER PROFILE VIEW
 @api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/programs/',
-        '/api/programs/<id>/',
-        '/api/programs/<id>/<id>/',
-    ]
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
 
-    return Response(routes)
+    serializer = UserSerializer(user, many=False)
+
+    return Response(serializer.data)
+
+#View All Users
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+
+    return Response(serializer.data)
 
 #PROGRAMS VIEW
 @api_view(['GET'])
